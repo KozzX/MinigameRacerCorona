b7fd51b2e142b0e45f576b9dcedd9b987105ee1b
KozzX <andre@andrecandido.com>
2015-03-03 06:29:13 -0300

    Commit

--- config.lua
+++ config.lua
@@ -18,6 +18,13 @@ application =
 		},
 		--]]
 	},
+	license =
+    {
+        google =
+        {
+            key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAn+SQCsE20Wc6O2PXTMLcpwIC+7GU8XqSpYwsVXArFv0879v5npjF1BXiCBJwSlx6vF5fmcFlgd7amTHX3WEBL9I5SLL8+368EMwQjgr5v19g++EtgCgI3fCyjVZNmA2FoanfJH+y2oTLZ1CX1poecIrhdQFCRSPkiEY0kyvI8xGOHRGiaelHA/yLzcX8GSVHyIst3rg4gK1ZUpBr8TmlqJKsi2q2ioHVJRdfOi0zCmZfbbpomBoTV0JI8GfnYpz71pOqKz8iVJZw0LS6uZoVuGZGFM5aSMkzJ8RBNW3fmIGVgqFSpAd3A5+jTV8h/wfUwY/N4GU24P7iM0vQVz0MSQIDAQAB",
+        },
+    },
 
 	--[[
 	-- Push notifications
